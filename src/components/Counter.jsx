65774db64532a743f0d90a632c599bbf1c17b83a
Toscan <dipointg@gmail.com>
2023-04-26 11:08:16 +0200

    counter created

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement, incrementByAmount } from "./counterSlice.js";
+import { increment, decrement, incrementByAmount } from "./counterSlice";
 
 const myStyle = {
   color: "red",
@@ -11,29 +11,33 @@ export default function Counter() {
 
 // Hooks
 const counterState = useSelector((state)=> state.counter);
+const dispatch = useDispatch();
+
+let {value} = counterState
 
 
 // Handlers
 const handleIncrement = ()=>{
   // handle increment
-
+dispatch(increment());
 };
 const handleDecrement = ()=>{
   // handle decrement
-
+  dispatch(decrement());
 };
 const handleIncrementByAmount = ()=>{
   // handle increment by amount
-
+  dispatch(incrementByAmount(10));
 };
 
   return (
     <div>
       <h2>
-        Counter #1: <span style={myStyle}>({counterState.value})</span>
+        Counter #1: <span style={myStyle}>({value})</span>
       </h2>
-      <button onClick={handleIncrement}>+</button>
-      <button onClick={handleDecrement}>-</button>
+      {/* <p>name: {counterSlice.name}</p> */}
+      <button onClick={handleIncrement}>+1</button>
+      <button onClick={handleDecrement}>-1</button>
       <button onClick={handleIncrementByAmount}>+10</button>
     </div>
   );
