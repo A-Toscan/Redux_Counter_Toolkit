086a10af4e13641e8fb94b4045f9375cf2d93d0a
Toscan <dipointg@gmail.com>
2023-04-26 10:33:41 +0200

    creating the hooks and handlers

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, incrementByAmount } from "./counterSlice.js";
 
 const myStyle = {
   color: "red",
@@ -6,11 +8,33 @@ const myStyle = {
 };
 
 export default function Counter() {
+
+// Hooks
+const counterState = useSelector((state)=> state.counter);
+
+
+// Handlers
+const handleIncrement = ()=>{
+  // handle increment
+
+};
+const handleDecrement = ()=>{
+  // handle decrement
+
+};
+const handleIncrementByAmount = ()=>{
+  // handle increment by amount
+
+};
+
   return (
     <div>
       <h2>
-        Counter #1: <span style={myStyle}>({1})</span>
+        Counter #1: <span style={myStyle}>({counterState.value})</span>
       </h2>
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleDecrement}>-</button>
+      <button onClick={handleIncrementByAmount}>+10</button>
     </div>
   );
 }
